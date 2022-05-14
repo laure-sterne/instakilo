f054ef54b90f958c7c2592f8aa7af231a79e8805
laure-sterne <laureuh@LAPTOP-ON7TH49O.localdomain>
2022-05-14 14:23:44 +0200

    creation first component kiloPhoto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KiloPhotoComponent } from './kilo-photo/kilo-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KiloPhotoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/kilo-photo/kilo-photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kilo-photo',
+  templateUrl: './kilo-photo.component.html',
+  styleUrls: ['./kilo-photo.component.scss']
+})
+export class KiloPhotoComponent {
+  
+}
